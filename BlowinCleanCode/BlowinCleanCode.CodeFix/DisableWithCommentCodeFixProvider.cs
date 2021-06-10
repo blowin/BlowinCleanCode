b4a5064fe0af64fd294a4e2935846a88c741ca9d
blowin <dsessence@gmail.com>
2021-06-10 08:32:34 +0300

    add empty code fix project

---
+++ BlowinCleanCode/BlowinCleanCode.CodeFix/DisableWithCommentCodeFixProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace BlowinCleanCode.CodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DisableWithCommentCodeFixProvider)), Shared]
+    public class DisableWithCommentCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray<string>.Empty;
+        
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
