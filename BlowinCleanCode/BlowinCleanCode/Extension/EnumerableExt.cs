0bd2537394a4c0852617342f1b82939de3d12239
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2022-02-03 08:19:21 +0300

    simplify check: IsAdapterCall

--- BlowinCleanCode/BlowinCleanCode/Extension/EnumerableExt.cs
+++ BlowinCleanCode/BlowinCleanCode/Extension/EnumerableExt.cs
@@ -10,5 +10,45 @@ namespace BlowinCleanCode.Extension
         {
             yield return self;
         }
+
+        /// <summary>
+        /// If has no elements, tuple = null
+        /// If has less, than 2 value, than second = default
+        /// otherwise first = firstElement, second = secondElement
+        ///
+        /// For example:
+        ///     input seq [] = null
+        ///     input seq ["Hello"] = ("Hello", null)
+        ///     input seq ["Hello", "World"] = ("Hello", "World")
+        ///     input seq ["Hello", "World", "Third", ...] = ("Hello", "World")
+        /// </summary>
+        /// <returns></returns>
+        public static (T first, T second)? FirstPairOrDefault<T>(this IEnumerable<T> self)
+        {
+            T first = default;
+            T second = default;
+            int number = 0;
+            foreach (var item in self)
+            {
+                number += 1;
+                if (number == 1)
+                {
+                    first = item;
+                }
+                else if (number == 2)
+                {
+                    second = item;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (number == 0)
+                return null;
+
+            return (first, second);
+        }
     }
 }
--- BlowinCleanCode/BlowinCleanCode/Feature/CodeSmell/MiddleManFeatureAnalyze.cs
+++ BlowinCleanCode/BlowinCleanCode/Feature/CodeSmell/MiddleManFeatureAnalyze.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Linq;
 using BlowinCleanCode.Extension;
 using BlowinCleanCode.Extension.SymbolExtension;
@@ -24,71 +23,76 @@ namespace BlowinCleanCode.Feature.CodeSmell
         
         protected override void Analyze(SyntaxNodeAnalysisContext context, TypeDeclarationSyntax syntaxNode)
         {
-            if(!context.ContainingSymbol.Is<INamedTypeSymbol>(out var namedTypeSymbol) || syntaxNode.TypeName().EndsWith("Adapter"))
+            if (Skip(context, syntaxNode, out var namedTypeSymbol, out var fieldSymbol)) 
                 return;
 
-            if (namedTypeSymbol.IsStatic)
-                return;
-            
             var methods = namedTypeSymbol.Methods(false)
                 .AsSyntax<MethodDeclarationSyntax>()
                 .ToImmutableArray();
             
-            if(!AllMethodsUseOneFieldWithSingleCall(namedTypeSymbol, methods, context, out var field))
+            if(!AllMethodsUseOneFieldWithSingleCall(methods, fieldSymbol, context))
                 return;
             
-            ReportDiagnostic(context, syntaxNode.Identifier.GetLocation(), syntaxNode.TypeName(), field.Name);
+            ReportDiagnostic(context, syntaxNode.Identifier.GetLocation(), syntaxNode.TypeName(), fieldSymbol.Name);
         }
 
-        private static bool AllMethodsUseOneFieldWithSingleCall(INamedTypeSymbol parentType, 
-            ImmutableArray<MethodDeclarationSyntax> methods, 
-            SyntaxNodeAnalysisContext context,
-            out ISymbol useField)
+        private static bool Skip(SyntaxNodeAnalysisContext context, TypeDeclarationSyntax syntaxNode,
+            out INamedTypeSymbol namedTypeSymbol,
+            out IFieldSymbol fieldSymbol)
         {
-            useField = default;
+            fieldSymbol = default;
+            if (!context.ContainingSymbol.Is(out namedTypeSymbol) || syntaxNode.TypeName().EndsWith("Adapter"))
+                return true;
 
-            var set = new HashSet<ISymbol>(SymbolEqualityComparer.Default);
-            var nodes = new List<SyntaxNode>(2);
-            foreach (var methodDeclarationSyntax in methods)
-            {
-                nodes.Clear();
-                nodes.AddRange(methodDeclarationSyntax.GetBodyChildNodes().Take(2));
-                if(nodes.Count == 0)
-                    continue;
+            if (namedTypeSymbol.IsStatic)
+                return true;
 
-                if (nodes.Count > 1)
-                    return false;
+            var pair = namedTypeSymbol.Fields().Where(e => !e.IsConst).FirstPairOrDefault();
+            if (pair == null)
+                return true;
 
-                var node = nodes[0];
-                if (node is ReturnStatementSyntax returnStatementSyntax)
-                    node = returnStatementSyntax.Expression;
-                
-                if(node == null)
-                    continue;
-
-                if (!IsAdapterCall(context, node, set)) 
-                    return false;
-            }
-
-            useField = set.SingleOrDefault();
-            if (useField == null)
-                return false;
-
-            foreach (var fieldSymbol in parentType.Fields())
-            {
-                if(fieldSymbol.IsConst)
-                    continue;
-
-                if (SymbolEqualityComparer.Default.Equals(fieldSymbol, useField))
-                    return true;
-            }
+            var (first, second) = pair.Value;
+            if (second != null) 
+                return true;
             
+            fieldSymbol = first;
             return false;
         }
 
-        private static bool IsAdapterCall(SyntaxNodeAnalysisContext context, SyntaxNode node,
-            HashSet<ISymbol> set)
+        private static bool AllMethodsUseOneFieldWithSingleCall(ImmutableArray<MethodDeclarationSyntax> methods, ISymbol typeField,
+            SyntaxNodeAnalysisContext context)
         {
+            var hasAnyCall = false;
+            
+            foreach (var methodDeclarationSyntax in methods)
+            {
+                var firstPairOrDefault = methodDeclarationSyntax.GetBodyChildNodes().FirstPairOrDefault();
+                if(firstPairOrDefault == null)
+                    continue;
+
+                var (firstNode, second) = firstPairOrDefault.Value;
+                // more than 2 item
+                if (second != null)
+                    return false;
+                
+                if (firstNode is ReturnStatementSyntax returnStatementSyntax)
+                    firstNode = returnStatementSyntax.Expression;
+                
+                if(firstNode == null)
+                    continue;
+
+                if (!IsAdapterCall(firstNode, typeField, context)) 
+                    return false;
+
+                hasAnyCall = true;
+            }
+
+            return hasAnyCall;
+        }
+
+        private static bool IsAdapterCall(SyntaxNode node, ISymbol typeField, SyntaxNodeAnalysisContext context)
+        {
+            // if not simple call, like this: obj.Run();
             if (!node.Is<InvocationExpressionSyntax>(out var invocationExpressionSyntax) ||
                 !invocationExpressionSyntax.Expression.Is<MemberAccessExpressionSyntax>(out var memberAccessExpressionSyntax) ||
                 !memberAccessExpressionSyntax.Expression.Is<IdentifierNameSyntax>(out var identifierNameSyntax))
@@ -100,11 +104,7 @@ namespace BlowinCleanCode.Feature.CodeSmell
             if (field == null)
                 return false;
 
-            set.Add(field);
-            if (set.Count > 1)
-                return false;
-            
-            return true;
+            return SymbolEqualityComparer.Default.Equals(field, typeField);
         }
     }
 }
